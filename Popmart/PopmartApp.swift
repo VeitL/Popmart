89df137b639452149cab4d54aa3a4fe23307d26a
Fynn <guanchen.li.119@gmail.com>
2025-05-29 16:04:32 +0200

    Initial Commit

---
+++ Popmart/PopmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopmartApp.swift
+//  Popmart
+//
+//  Created by Guanchenulous on 29.05.25.
+//
+
+import SwiftUI
+
+@main
+struct PopmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PopmartTests/PopmartTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopmartTests.swift
+//  PopmartTests
+//
+//  Created by Guanchenulous on 29.05.25.
+//
+
+import Testing
+@testable import Popmart
+
+struct PopmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
